898289519efe13d91d2d331cdaea946907ebf35d
Radek the Dude <studio@pythonforce.com>
2022-07-15 19:37:21 -0400

    change url import and re_path

--- hide_herokuapp/urls.py
+++ hide_herokuapp/urls.py
@@ -2,11 +2,11 @@
 try:
     from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import url
+    from django.urls import re_path
 
 from hide_herokuapp.views import herokuapp_robots_view
 
 
 urlpatterns = [
-    url(r'^robots\.txt$', herokuapp_robots_view),
+    re_path(r'^robots\.txt$', herokuapp_robots_view),
 ]
